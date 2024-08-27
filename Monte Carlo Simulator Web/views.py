cf6cc129ae83cc0c723bd5efd37cbf083c1a935c
Ben Crittenden <130415312+BenCritt@users.noreply.github.com>
2024-08-27 16:01:47 -0500

    Update views.py

--- Monte Carlo Simulator Web/views.py
+++ Monte Carlo Simulator Web/views.py
@@ -1,33 +1,34 @@
-from pathlib import Path
-import numpy as np
-import matplotlib.pyplot as plt
-from .forms import MonteCarloForm
-from django.shortcuts import render, redirect
-from django.urls import reverse
-import os
-from django.http import HttpResponse
-
-
+# This is the code for the Monte Carlo Simulator.
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def monte_carlo_simulator(request):
+    # Check if the request method is POST.
     if request.method == "POST":
+        # Initialize the form with data from the request.
         form = MonteCarloForm(request.POST)
+        # Validate the form
         if form.is_valid():
             # This determines where to save the PDF file that will eventually be created.
             home_dir = os.path.expanduser("~")
             save_dir = os.path.join(home_dir, "Downloads")
+            # Create the directory if it doesn't exist.
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
+            # Create a filename and define the full path of the directory on the user's computer.
             filename = "probability_graph.pdf"
             full_path = os.path.join(save_dir, filename)
 
-            # This pulls the data from the HTML to prepare for the graph generation.
+            # This pulls the data from the first HTML form to prepare for the graph generation.
             sim_quantity = form.cleaned_data["sim_quantity"]
             min_val = form.cleaned_data["min_val"]
             max_val = form.cleaned_data["max_val"]
             target_val = form.cleaned_data["target_val"]
 
+            # Generate random data for the first simulation.
             sim_result = np.random.uniform(min_val, max_val, sim_quantity)
+
+            # Check for a second simulation.
             second_sim_quantity = form.cleaned_data["second_sim_quantity"]
+
             # Begin second data range, if there is one.
             if form.cleaned_data["second_sim_quantity"] is not None:
                 second_min_val = form.cleaned_data["second_min_val"]
@@ -54,29 +55,33 @@ def monte_carlo_simulator(request):
                     )
                 plt.savefig(full_path, format="pdf")
 
+                # Generate a response with the generated PDF.
                 with open(full_path, "rb") as f:
                     response = HttpResponse(f.read(), content_type="pdf")
-                    response[
-                        "Content-Disposition"
-                    ] = 'attachment; filename="probability_graph.pdf"'
+                    response["Content-Disposition"] = (
+                        'attachment; filename="probability_graph.pdf"'
+                    )
                     return response
-
+            # Handle the case where there is only one simulation.
             elif form.cleaned_data["second_sim_quantity"] is None:
                 plt.figure()
                 plt.hist(sim_result, density=True, edgecolor="white")
                 plt.axvline(target_val, color="r")
                 plt.savefig(full_path, format="pdf")
 
+                # Generate a response with the generated PDF.
                 with open(full_path, "rb") as f:
                     response = HttpResponse(f.read(), content_type="pdf")
-                    response[
-                        "Content-Disposition"
-                    ] = 'attachment; filename="probability_graph.pdf"'
+                    response["Content-Disposition"] = (
+                        'attachment; filename="probability_graph.pdf"'
+                    )
                     return response
 
+    # Create an empty form for GET request.
     else:
         form = MonteCarloForm()
 
+    # Render the Monte Carlo simulator page with the form.
     return render(
         request, "projects/monte_carlo_simulator.html", context={"form": form}
     )
